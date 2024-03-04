02bead1deb6ee6cfb1a4dc9346d0817d1ea0b017
Richard Paterson <45054767+Station95@users.noreply.github.com>
2024-03-05 11:23:58 +1300

    temp remove the js for confirmation its not required

--- src/Pages/MemberProfilePageController.php
+++ src/Pages/MemberProfilePageController.php
@@ -714,7 +714,7 @@ class MemberProfilePageController extends PageController
             }
 
             if ($name === 'Password') {
-                Requirements::javascript("silverstripe/admin: thirdparty/jquery/jquery.js");
+                // Requirements::javascript("silverstripe/admin: thirdparty/jquery/jquery.js");
                 Requirements::javascript("symbiote/silverstripe-memberprofiles: client/javascript/ConfirmedPasswordField.js");
             }
 
